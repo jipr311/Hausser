3232c0a4115000cc99093c5f82901ed96703a800
Jose Pereira <jose.pereira@gigatronik.com>
2014-11-27 12:33:20 +0100

    Project LogMessages.

--- core/src/co/ve/jipr/mygdx/test/screens/EndBmwAnimation.java
+++ core/src/co/ve/jipr/mygdx/test/screens/EndBmwAnimation.java
@@ -12,6 +12,13 @@ public class EndBmwAnimation implements Screen {
 	@Override
 	public void render(float delta) {
 		Gdx.app.setLogLevel(Application.LOG_INFO);
+		/**
+		 * where logLevel can be one of the following values:
+		 * Application.LOG_NONE: mutes all logging.
+		 * Application.LOG_DEBUG: logs all messages.
+		 * Application.LOG_ERROR: logs only error messages.
+		 * Application.LOG_INFO: logs error and normal messages.
+		 */
 		Gdx.app.log(TAG, "my informative message");
 		Gdx.app.error(TAG, "my error message");
 		Gdx.app.debug(TAG, "my debug message");
